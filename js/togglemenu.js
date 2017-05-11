55226c3c20f77263b4ce4b1d1fe2fcd6c497877a
altheatan <altheakay.virgoventures@gmail.com>
2017-05-12 00:15:39 +0800

    changes

---
+++ js/togglemenu.js
@@ -0,0 +1,12 @@
+//タブレットslider
+$(function(){
+	$('#btn_smenu').click(function(){
+				$('#smenu').slideToggle();
+	});
+	$(window).resize(function(){
+		var tw = $('#contents').width();
+		if(640 < tw) {
+			$('#smenu').css('display','none');
+		}
+	});
+});
